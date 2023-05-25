b2d0e44facb3f757ae37707caf209c2324a1f818
ibrahim-tekfidan <ibrahim.tekfidn@gmail.com>
2023-05-25 14:49:54 +0300

    pagination feature added

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,42 +1,78 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import Pagination from './pagination';
 
 class Note extends Component {
-  state = {};
-  render() {
-    let { notes } = this.props;
-    const { selectedCategory, onDelete, categories } = this.props;
+  state = {
+    currentPage: 1,
+    pageSize: 5,
+  };
 
-    notes = selectedCategory
-      ? notes.filter(n => n.category === selectedCategory)
-      : notes;
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    const { pageSize, currentPage } = this.state;
+    const { onDelete, categories } = this.props;
+
+    // Kategorilere ayırmak için
+    let categoriseNotes = this.categoriseNote();
+    let notes = categoriseNotes;
+
+    // Sayfalama yapmak için
+    if (categoriseNotes.length >= pageSize)
+      notes = this.paginateNote(categoriseNotes);
 
     return (
-      <ul>
-        {notes.map(note => (
-          <li key={note.id} className="note">
-            <p>
-              {note.text}
-              <a className="source" href={note.source} target="_blank">
-                (Source)
-              </a>
-            </p>
-            <span
-              style={{
-                backgroundColor: categories.find(
-                  category => category.name === note.category
-                ).color,
-              }}
-              className="tag"
-            >
-              {note.category}
-            </span>
-            <div className="complete-button">
-              <button onClick={() => onDelete(note)}>Tamamla ✅</button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul>
+          {notes.map(note => (
+            <li key={note.id} className="note">
+              <p>
+                {note.text}
+                <a className="source" href={note.source} target="_blank">
+                  (Source)
+                </a>
+              </p>
+              <span
+                style={{
+                  backgroundColor: categories.find(
+                    category => category.name === note.category
+                  ).color,
+                }}
+                className="tag"
+              >
+                {note.category}
+              </span>
+              <div className="complete-button">
+                <button onClick={() => onDelete(note)}>Tamamla ✅</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <Pagination
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          notesCount={categoriseNotes}
+        />
+      </>
     );
+  }
+
+  categoriseNote() {
+    const { notes, selectedCategory } = this.props;
+
+    return selectedCategory
+      ? notes.filter(n => n.category === selectedCategory)
+      : notes;
+  }
+
+  paginateNote(notes) {
+    const { currentPage, pageSize } = this.state;
+    const staterIndex = (currentPage - 1) * pageSize;
+    return _(notes).slice(staterIndex).take(pageSize).value();
   }
 }
 
---
+++ src/components/pagination.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+class Pagination extends Component {
+  state = {};
+
+  render() {
+    const { notesCount, pageSize, onPageChange, currentPage } = this.props;
+
+    const pagesCount = Math.ceil(notesCount.length / pageSize);
+    if (pagesCount === 1) return null;
+    const pages = _.range(1, pagesCount + 1);
+
+    return (
+      <ul className="pagination">
+        {pages.map(page => (
+          <li key={page}>
+            <button
+              className={this.formatButtonClass(page, currentPage)}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  formatButtonClass(page, currentPage) {
+    let classes = 'btn-page';
+    classes += page === currentPage ? ' btn-page-active' : '';
+    return classes;
+  }
+}
+
+export default Pagination;
